08185142617d2ba32559df918749a7b01e68a0e4
Peter Griess <pg@std.in>
2022-02-18 16:25:51 -0600

    Add API class and albums() method

--- pixstar/api.py
+++ pixstar/api.py
@@ -1,4 +1,58 @@
+from http.cookiejar import CookieJar
 import lxml.etree
+from urllib.parse import urlencode
+import urllib.request
+
+
+class API:
+    cookie_jar = None
+    csrf_token = None
+    url_opener = None
+
+    def __init__(self, ssl_context=None):
+        self.cookie_jar = CookieJar()
+        self.url_opener = urllib.request.build_opener(
+            urllib.request.HTTPCookieProcessor(self.cookie_jar),
+            urllib.request.HTTPSHandler(context=ssl_context))
+
+    def login(self, username, password):
+        '''
+        Login to the Pix-Star service.
+
+        This method must be invoked before any other methods.
+        '''
+
+        assert not self.csrf_token
+
+        self.url_opener.open('https://www.pix-star.com/')
+        for c in self.cookie_jar:
+            if c.name == 'csrftoken':
+                self.csrf_token = c.value
+
+        assert self.csrf_token
+
+        resp = self.url_opener.open(
+            'https://www.pix-star.com/accounts/login/',
+            data=urllib.parse.urlencode({
+                'csrfmiddlewaretoken': self.csrf_token,
+                'username': username,
+                'password': password}).encode('utf-8'))
+
+        assert resp.status == 200
+        assert resp.geturl() == 'https://www.pix-star.com/my_pixstar/'
+
+    def albums(self):
+        '''
+        List the user's web albums.
+        '''
+
+        assert self.csrf_token
+
+        resp = self.url_opener.open('https://www.pix-star.com/albums/list/')
+        assert resp.status == 200
+
+        return _parse_list_response(resp)
+
 
 def _parse_list_response(f):
     '''
--- pixstar/main.py
+++ pixstar/main.py
@@ -2,21 +2,13 @@ from argparse import ArgumentParser
 from datetime import datetime
 from datetime import timedelta
 from datetime import timezone
-from http.cookiejar import CookieJar
 import logging
-import lxml.etree
 from ssl import CERT_NONE, SSLContext
 import sys
-from urllib.parse import urlencode
-import urllib.request
 
 import osxphotos
-from parso import parse
 
-
-class HTTPNoRedirectHandler(urllib.request.HTTPRedirectHandler):
-    def redirect_request(self, req, fp, code, msg, headers, url):
-        return None
+from .api import API
 
 
 def main():
@@ -38,38 +30,17 @@ def main():
         style='{', format='{message}', stream=sys.stderr,
         level=logging.ERROR - args.verbosity * 10)
 
-    ctx = SSLContext()
-    ctx.verify_mode = CERT_NONE
+    # TODO: Move this behind an option, or figure out how to get Charles
+    #       certificates imported into the system keychain. Try for the second.
+    # ctx = SSLContext()
+    # ctx.verify_mode = CERT_NONE
+    ctx = None
 
-    cj = CookieJar()
-    opener = urllib.request.build_opener(
-        urllib.request.HTTPCookieProcessor(cj),
-        urllib.request.HTTPSHandler(context=ctx))
-    opener.open('https://www.pix-star.com/')
-    csrftoken = None
-    for c in cj:
-        if c.name == 'csrftoken':
-            csrftoken = c.value
+    api = API(ssl_context=ctx)
+    api.login(args.username, args.password)
 
-    assert csrftoken
-    logging.info(f'Found CSRF token: {csrftoken}')
-
-    resp = opener.open(
-        'https://www.pix-star.com/accounts/login/',
-        data=urllib.parse.urlencode({
-            'csrfmiddlewaretoken': csrftoken,
-            'username': args.username,
-            'password': args.password}).encode('utf-8'))
-
-    assert resp.status == 200
-    assert resp.geturl() == 'https://www.pix-star.com/my_pixstar/'
-
-    logging.info('Logged in')
-
-    resp = opener.open('https://www.pix-star.com/albums/list/')
-    assert resp.status == 200
-
-    doc = lxml.etree.parse(resp, lxml.etree.HTMLParser())
+    for a in api.albums():
+        print(a)
 
     if False:
         db_path = osxphotos.utils.get_system_library_path()
